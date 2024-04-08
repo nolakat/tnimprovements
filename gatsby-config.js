6f9cc1699903afc3f9faaf6e44f29cef92193014
nolakat <nolakat@gmail.com>
2024-04-07 22:00:53 -0400

    GA Updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,36 +57,10 @@ module.exports = {
             // Avoids sending pageview hits from custom paths
             exclude: ["/preview/**", "/do-not-track/me/too/"],
             // Defaults to https://www.googletagmanager.com
-            origin: "YOUR_SELF_HOSTED_ORIGIN",
+            origin: "https://tnimprovements.com/",
             // Delays processing pageview events on route update (in milliseconds)
             delayOnRouteUpdate: 0,
           },
-        },
-      },
-      {
-        resolve: `gatsby-plugin-google-analytics`,
-        options: {
-          // The property ID; the tracking code won't be generated without it
-          trackingId: "G-56FX3YX2Y3",
-          // Defines where to place the tracking script - `true` in the head and `false` in the body
-          head: false,
-          // Setting this parameter is optional
-          anonymize: true,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Delays sending pageview hits on route update (in milliseconds)
-          pageTransitionDelay: 0,
-          // Set Variation ID. 0 for original 1,2,3....
-          variationId: "0",
-          // Defers execution of google analytics script after page load
-          defer: false,
-          // Any additional optional fields
-          sampleRate: 5,
-          siteSpeedSampleRate: 10,
-          // defaults to false
-          enableWebVitalsTracking: true,
         },
       },
     ]
