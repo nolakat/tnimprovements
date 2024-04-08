2c3c8fcc58419b5b9868ff97977689488aae6ba6
nolakat <nolakat@gmail.com>
2024-04-07 22:10:06 -0400

    GA Update Two

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,15 +51,7 @@ module.exports = {
           // This object is used for configuration specific to this plugin
           pluginConfig: {
             // Puts tracking script in the head instead of the body
-            head: false,
-            // Setting this parameter is also optional
-            respectDNT: true,
-            // Avoids sending pageview hits from custom paths
-            exclude: ["/preview/**", "/do-not-track/me/too/"],
-            // Defaults to https://www.googletagmanager.com
-            origin: "https://tnimprovements.com/",
-            // Delays processing pageview events on route update (in milliseconds)
-            delayOnRouteUpdate: 0,
+            head: true
           },
         },
       },
